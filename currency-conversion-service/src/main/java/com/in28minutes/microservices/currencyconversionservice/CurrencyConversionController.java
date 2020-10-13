db2c9e7e5894a4ca4c2f49632ce3fea9e8897abc
uriel melendez <uriel.melendez@outlook.es>
2020-10-13 18:44:23 -0500

    Adding Ribbon and Feign implementation

--- currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +14,10 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class CurrencyConversionController {
 
+	@Autowired
+	private CurrencyExchangeServerProxy proxy;
+	
+	
 	@GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversionBean convertCurrency(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
 		
@@ -26,6 +31,17 @@ public class CurrencyConversionController {
 				uriVariables);
 	
 	   CurrencyConversionBean response= responseEntity.getBody();
+		
+		return new CurrencyConversionBean(response.getId(), from,to,response.getConvertionMultiple(),
+				quantity,quantity.multiply(response.getConvertionMultiple()),response.getPort());
+	}
+	
+	
+	//Feign - Rest client
+	@GetMapping("/currency-converter-feign/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConversionBean convertCurrencyFeign(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
+		
+	   CurrencyConversionBean response= proxy.retrieveExchangeValue(from, to);
 		
 		return new CurrencyConversionBean(response.getId(), from,to,response.getConvertionMultiple(),
 				quantity,quantity.multiply(response.getConvertionMultiple()),response.getPort());
